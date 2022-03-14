46dce20c472bfe5977f919eab58273f2d0203ca6
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-03-15 01:59:20 +0600

    searchButton function work has  been done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,24 @@
-alert('He')
+// Load Main Container
+const main = document.getElementById('mainContainer');
+
+
+// Search work
+const searchButton = () =>{
+    const input = document.getElementById('search-input');
+    const error = document.getElementById('error');
+    const inputValue = input.value;
+    if(inputValue ==""){
+        error.innerText = 'Please give Phone Name';
+        input.value = '';
+        main.innerHTML = '';
+
+    }else {
+        main.innerHTML = '';
+        fetch('https://openapi.programming-hero.com/api/phones?search=${inputValue}')
+        .then(res => res.json())
+        .then(data => phoneDisplay(data.phones))
+        input.value = '';
+        main.innerHTML = '';
+    }
+
+}
