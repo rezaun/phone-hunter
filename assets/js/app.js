3f5d842a46e333899b02141f60997d7d89f8564e
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-03-15 02:21:48 +0600

    Search phone  successfully done

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,11 +14,31 @@ const searchButton = () =>{
 
     }else {
         main.innerHTML = '';
-        fetch('https://openapi.programming-hero.com/api/phones?search=${inputValue}')
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
         .then(res => res.json())
-        .then(data => phoneDisplay(data.phones))
+        .then(data => phoneDisplay(data.data))
         input.value = '';
         main.innerHTML = '';
     }
 
+}
+
+const phoneDisplay = (phones) =>{
+    for(const phone of phones){
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col-sm-4');
+        div.innerHTML = ` 
+        <div class="card" style="width: 18rem;">
+            <img src="${phone.image}" class="card-img-top" alt="${phone.phone_name}">
+            <div class="card-body">
+            <h6 class="card-title">brand Name: ${phone.brand}</h6>
+            <h5 class="card-title">Phone Name: ${phone.phone_name}</h5>            
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="${phone.slug}" class="btn btn-primary">Phone Details</a>
+            </div>
+        </div>
+        `;
+        main.appendChild(div);
+    }
 }
