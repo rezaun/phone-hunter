d7e06e6ed3d49e6484eecdeb74847d5493c2373d
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-03-16 02:43:34 +0600

    no phone found and phone details view change

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,20 @@
 // Load Containers
 const main = document.getElementById('mainContainer');
 
-
-
 // Search work
 const searchButton = () =>{
     const input = document.getElementById('search-input');
+    const detailsMain   = document.getElementById('detailsContainer');
+    detailsMain.innerHTML = '';
+    document.getElementById('spinner').style.display = 'block';
     const error = document.getElementById('error');
-    const inputValue = input.value;
-    if(inputValue ==""){
+    const inputValue = (input.value).toLowerCase();    
+    if(inputValue ==""){        
         error.innerText = 'Please give Phone Name or Model';
         input.value = '';
         main.innerHTML = '';
-
-    }else {
-        main.innerHTML = '';
+        return;
+    } else {        
         fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
         .then(res => res.json())
         .then(data => phoneDisplay(data.data))
@@ -25,8 +25,17 @@ const searchButton = () =>{
 }
 
 const phoneDisplay = (phones) =>{
+    document.querySelector('#spinner').style.display = 'none';
+    const error = document.getElementById('error');    
+    if ( phones.length < 1 ) {
+        error.innerText = 'No Phone Found';
+        return;   
+    }
+
+    error.innerText = '';
+    
     for(const phone of phones){
-        console.log(phone);
+        //console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col-sm-4');
         div.innerHTML = ` 
@@ -34,8 +43,7 @@ const phoneDisplay = (phones) =>{
             <img src="${phone.image}" class="card-img-top" alt="${phone.phone_name}">
             <div class="card-body">
             <h6 class="card-title">brand Name: ${phone.brand}</h6>
-            <h5 class="card-title">Phone Name: ${phone.phone_name}</h5>            
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 class="card-title">Phone Name: ${phone.phone_name}</h5> 
             <button onclick="loadPhoneDetail('${phone.slug}')" class="btn btn-primary">Phone Details</button>
             </div>
         </div>
@@ -55,19 +63,68 @@ const loadPhoneDetail= phoneId =>{
 
 const displayPhoneDetail = phone => {
     const detailsMain   = document.getElementById('detailsContainer');
-    const div = document.createElement('div');
-    
+    const div = document.createElement('div');    
     div.classList.add('col');
     div.classList.add('text-center');
     div.innerHTML = `
-        <img src="${phone.image}" class="card-img-top w-25" alt="${phone.name}">
-        <div class="card-body">
-        <h4>Phone Others Options</h4>
-        <h5 class="card-title">Phone Name:<b> ${phone.name}</b></h5>
-        <pclass="card-text">Phone USB:<b> ${phone.others.USB}</b></p>
-        <p class="card-text">BlueTooth:<b> ${phone.others.Bluetooth}</b> </p>
-        <p class="card-text">Radio:<b> ${phone.others.Radio}</b></p>
-        <p>Release Date <b>${phone.releaseDate ? phone.releaseDate : 'No release Date Found'} </b></p>       
+        <table class="table table-bordered border-primary">
+        <tr>
+            <th>Phone Image:</th>
+            <td><img src="${phone.image}" class="card-img-top w-25" alt="${phone.name}"></td>
+        </tr>
+        <tr>
+            <th>Phone Main Feature</th>
+            <th>Feature</th>
+        </tr>
+        <tr>
+            <th>Phone Name</th>
+            <td>${phone.mainFeatures.storage}</td>
+        </tr>
+        
+        <tr>
+            <th>Phone USB</th>
+            <td>${phone.mainFeatures.displaySize}</td>
+        </tr>
+        
+        <tr>
+            <th>Memory</th>
+            <td>${phone.mainFeatures.memory}</td>
+        </tr>
+        
+        <tr>
+            <th>Phone USB</th>
+            <td>${phone.mainFeatures.displaySize}</td>
+        </tr>
+        <tr>
+            <th>Phone Others Options</th>
+            <th>Options</th>
+        </tr>
+        <tr>
+            <th>Phone Name</th>
+            <td>${phone.name}</td>
+        </tr>
+      
+        <tr>
+            <th>Phone USB</th>
+            <td>${phone.others.USB}</td>
+        </tr>      
+        <tr>
+            <th>BlueTooth</th>
+            <td>${phone.others.Bluetooth}</td>
+        </tr>
+      
+        <tr>
+            <th>Radio</th>
+            <td>${phone.others.Radio}</td>
+        </tr>      
+      
+        <tr>
+            <th>Release Date</th>
+            <td>${phone.releaseDate ? phone.releaseDate : 'No release Date Found'}</td>
+        </tr>
+      
+        </table>
+          
     `;
     detailsMain.appendChild(div);
 
